8f2e6bf69454efdefc307c0941de34d4b88f6d08
Dominick Oddo <dominickoddo@gmail.com>
2016-06-11 19:15:49 -0700

    Initial Commit

---
+++ ChatApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChatApp
+//
+//  Created by Dominick Oddo on 6/11/16.
+//  Copyright © 2016 ILGOS LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChatApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChatApp
+//
+//  Created by Dominick Oddo on 6/11/16.
+//  Copyright © 2016 ILGOS LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
